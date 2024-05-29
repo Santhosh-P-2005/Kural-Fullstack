63af61ca619e3cdab7e055739a3c0aa7ed0ca612
Santhosh-P-2005 <santhosh090405@gmail.com>
2024-05-29 12:33:47 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,12 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.Atlas_url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected'))
+mongoose.connect(process.env.Atlas_url).then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
 const usersRouter = require('./routes/users');
